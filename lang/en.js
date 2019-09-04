1508a3b9bc360ce32c3630197e2d16dab9597fcf
Maxime Cousinou <maxime.cousinou@zemax.net>
2019-09-04 16:09:02 +0200

    change default texts and include siteName next to privacyURL

--- lang/en.js
+++ lang/en.js
@@ -1,13 +1,12 @@
-const getText = ( { privacyURL } ) => ( id ) => ({
-	'banner':          () => `By clicking on Accept, you consent to the deposit of internal and third party cookies intended for navigation on the website, to the possible identification, to carry out statistics, to propose to you possibly offers adapted to your centers of interests, and to linking with social networks / websites. To view the data protection policy, click on&nbsp;: <a href="${privacyURL}">Read more&nbsp;+</a>`,
+const getText = ( { siteName, privacyURL } ) => ( id ) => ({
+	'banner':          () => `We use cookies, including third-party cookies, to provide you with the best possible navigation and to measure the frequency of our services. To find out more about our personal data protection policy, we invite you to <a href="${privacyURL}">click here</a>.`,
 	'accept':          () => 'Accept',
 	'deny':            () => 'Deny',
 	'customise':       () => 'Customise',
 	'details':         () => `<h2>Managing your preferences on cookies</h2>
 <p>The features of this site listed below are based on services offered by third parties.</p> 
-<p>If you give your consent, these third parties will deposit cookies that allow you to view content hosted by these third parties or to share our content.</p> 
-<p>Via these cookies, these third parties will collect and use your browsing data for their own purposes, in accordance with their privacy policy (link below).</p> 
-<p>This page allows you to give or withdraw your consent, either globally or purpose by purpose.</p>`,
+<p>If you give your consent, these third parties will deposit cookies that allow you to view on ${siteName} content hosted by these third parties or to share our content.</p> 
+<p>Via these cookies, these third parties will collect and use your browsing data for their own purposes, in accordance with their privacy policy (links below).</p>`,
 	'allServices':     () => 'Preferences for all services',
 	'policyLinkLabel': () => 'Link to the privacy policy',
 })[ id ]();
--- lang/fr.js
+++ lang/fr.js
@@ -1,13 +1,12 @@
-const getText = ( { privacyURL } ) => ( id ) => ({
-	'banner':          () => `En cliquant sur Accepter, vous consentez au dépôt de cookies internes et tiers destinés à la navigation sur le site internet, à l’identification éventuelle, à réaliser des statistiques, à vous proposer éventuellement des offres adaptées à vos centres d’intérêts, et à la mise en relation avec des réseaux sociaux/sites internet. Pour consulter la politique de protection des données, cliquez sur&nbsp;: <a href="${privacyURL}">En savoir&nbsp;+</a>`,
+const getText = ( { siteName, privacyURL } ) => ( id ) => ({
+	'banner':          () => `Nous utilisons des cookies, y compris des cookies tiers afin de vous proposer la meilleure navigation possible et de mesurer la fréquentation de nos services. Pour en savoir plus sur notre politique de protection des données personnelles, nous vous invitons à <a href="${privacyURL}">cliquer ici</a>.`,
 	'accept':          () => 'Accepter',
 	'deny':            () => 'Refuser',
 	'customise':       () => 'Personnaliser',
 	'details':         () => `<h2>Gestion de vos préférences sur les cookies</h2>
-<p>Les fonctionnalités de ce site listées ci-dessous s’appuient sur des services proposés par des tiers.</p> 
-<p>Si vous donnez votre accord (consentement), ces tiers déposeront des cookies qui vous permettront de visualiser du contenu hébergé par ces tiers ou de partager nos contenus.</p> 
-<p>Via ces cookies, ces tiers collecteront et utiliseront vos données de navigation pour des finalités qui leur sont propres, conformément à leur politique de confidentialité (lien ci-dessous).</p> 
-<p>Cette page vous permet de donner ou de retirer votre consentement, soit globalement soit finalité par finalité.</p>`,
+<p>Les fonctionnalités de ce site listées ci-dessous s’appuient sur des services proposés par des tiers (flux Twitter, vidéos, etc.).</p> 
+<p>Si vous donnez votre accord (consentement), ces tiers déposeront des cookies qui vous permettront de visualiser directement sur ${siteName} du contenu hébergé par ces tiers ou de partager nos contenus.</p> 
+<p>Via ces cookies, ces tiers collecteront et utiliseront vos données de navigation pour des finalités qui leur sont propres, conformément à leur politique de confidentialité (liens ci-dessous).</p>`,
 	'allServices':     () => 'Préférences pour tous les services',
 	'policyLinkLabel': () => 'Lien vers la politique de confidentialité',
 })[ id ]();
