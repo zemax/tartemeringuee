718844b96d434e426fff2796b23c296f6981c3ef
Maxime Cousinou <maxime.cousinou@zemax.net>
2020-07-01 16:31:34 +0200

    Enable IP anonimisation by default in Google Analytics

--- services/gtag.js
+++ services/gtag.js
@@ -1,24 +1,24 @@
 import addScript from "../utils/addscript";
 
-const service = ( UA, gtagMore = () => true ) => ({
-	'key':     'gtag',
-	'type':    'analytic',
-	'name':    'Google Analytics (gtag.js)',
-	'uri':     'https://support.google.com/analytics/answer/6004245',
-	'cookies': [ '_ga', '_gat', '_gid', '__utma', '__utmb', '__utmc', '__utmt', '__utmz', ('_gat_gtag_' + UA).replace( /-/g, '_' ) ],
-	'init':    () => {
-		window.dataLayer = window.dataLayer || [];
-
-		window.gtag = function gtag() {
-			window.dataLayer.push( arguments );
-		};
-
-		gtag( 'js', new Date() );
-		gtag( 'config', UA );
-		gtagMore();
-	},
-	'accept':  () => addScript( 'https://www.googletagmanager.com/gtag/js?id=' + UA ),
-	'refuse':  () => true,
+const service = ( UA, configOptions = { 'anonymize_ip': true }, gtagMore = () => true ) => ({
+    'key':     'gtag',
+    'type':    'analytic',
+    'name':    'Google Analytics (gtag.js)',
+    'uri':     'https://support.google.com/analytics/answer/6004245',
+    'cookies': [ '_ga', '_gat', '_gid', '__utma', '__utmb', '__utmc', '__utmt', '__utmz', ('_gat_gtag_' + UA).replace( /-/g, '_' ) ],
+    'init':    () => {
+        window.dataLayer = window.dataLayer || [];
+        
+        window.gtag = function gtag() {
+            window.dataLayer.push( arguments );
+        };
+        
+        gtag( 'js', new Date() );
+        gtag( 'config', UA, configOptions );
+        gtagMore();
+    },
+    'accept':  () => addScript( 'https://www.googletagmanager.com/gtag/js?id=' + UA ),
+    'refuse':  () => true,
 });
 
 export default service;
